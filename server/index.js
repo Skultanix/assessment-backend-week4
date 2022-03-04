bbfec8872082556b17babd4f26f1772e75e73e36
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-04 10:22:57 -0700

    Functionality one, 'Reveal Fortune' is complete.

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ app.use(cors());
 app.use(express.json()); // When we want to be able to accept JSON.
 
 app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
+  const compliments = [
+           "Gee, you're a smart cookie!",
 					 "Cool shirt!",
 					 "Your Javascript skills are stellar.",
   ];
@@ -19,6 +20,22 @@ app.get("/api/compliment", (req, res) => {
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
+  
+});
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes = [
+           "If you can learn to love yourself, you will never be lonely.",
+					 "If you wish to see the best in others, show the best of yourself.",
+					 "It is better to deal with problems before they arise.",
+           "If your desires are not extravagant, they will be granted."
+  ];
+
+  // choose random fortune
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+
+  res.status(200).send(randomFortune);
   
 });
 
